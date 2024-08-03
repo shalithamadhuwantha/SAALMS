ebe68bb8589359b2155c48f96a4eecd1afa872ff
Shalitha Madhuwantha <bbsmgamage@gmail.com>
2024-08-03 10:44:09 +0530

    main

--- app/components/LoginCard.tsx
+++ app/components/LoginCard.tsx
@@ -1,14 +1,17 @@
-
-import React from 'react';
+import React from "react";
 
 const Card: React.FC = () => {
   return (
-    
     <div className="flex items-center justify-center h-full w-full">
-      <div className="loginbg card w-4/5 h-4/5 bg-slate-700 shadow-xl rounded-lg">
-        <div className="card-body">
-          <div className="left card w-3/6 h-full bg-slate-300"></div>
-          <div className="right"></div>
+      <style jsx>{`
+        .cardbdr {
+          border-radius: 50px;
+        }
+      `}</style>
+      <div className="loginbg card w-4/5 h-4/6 bg-slate-700 shadow-xl rounded-e-3xl cardbdr">
+        <div className="card-body flex-row">
+          <div className="left card w-3/6 h-full bg-slate-300 flex-none "></div>
+          <div className="right card w-3/6 h-full bg-slate-300 flex-none  "></div>
         </div>
       </div>
     </div>
