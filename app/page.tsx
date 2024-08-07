48ac4800c0e64afcbc4a786d69e4f2cd8e0a20fe
Shalitha Madhuwantha <bbsmgamage@gmail.com>
2024-08-07 21:52:16 +0530

    shalitha

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ const App = () => {
               </p>
             </div>
             <p className="self-start ml-10 	text-2xl">
-              Use Your Google Account To logins
+              Use Your Google Account To login
             </p>
           </div>
           <Footer />
