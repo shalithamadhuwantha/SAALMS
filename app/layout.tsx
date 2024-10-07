f117020b6f2f6bf5554139bc8ee8717e67c7f081
Shalitha Madhuwantha <bbsmgamage@gmail.com>
2024-10-07 21:51:34 +0530

    web to PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,46 @@ import "./globals.css";
 import {NextauthProvider} from "./Providers"
 
 const inter = Inter({ subsets: ["latin"] });
+const APP_NAME = "SA&LMS";
+const APP_DEFAULT_TITLE = "SAALMS- Student Attendance & Lecture Management System";
+const APP_TITLE_TEMPLATE = "%s - SA&LMS";
+const APP_DESCRIPTION = "SAALMS- Student Attendance & Lecture Management System By Group 11";
+
 
 export const metadata: Metadata = {
-  title: "SAALMS- Student Attendance & Lecture Management System",
-  description: "SAALMS- Student Attendance & Lecture Management System By Group 11",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
